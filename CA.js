a8ca72a1436a6d301893613be0151dba11d02c83
Nhat0301 <96116589+Nhat0301@users.noreply.github.com>
2024-06-03 22:04:20 +0700

    Update CA.js

--- CA.js
+++ CA.js
@@ -1,6 +1,5 @@
 var objc = JSON.parse($response.body);
 objc = {
-    "id": 174494,
     "tax": 0.0,
     "total": 99.99,
     "total_currency": "USD",
@@ -15,5 +14,22 @@ objc = {
     ],
     "currency": 1,
     "tax_registration": null
+    "id": 200335,
+    "is_fully_paid": false,
+    "type": "recurring",
+    "created": "2024-06-03 14:44:20",
+    "number": "KPK7-TUFU-00C5-WE46",
+    "payment_system": "stripe",
+    "status": "requires_payment_method",
+    "client_secret": "pi_3PNc7UH8S8oSLzdf1DDrehQ3_secret_C4qLtKQGPNuYIZprG6QRnlk9z",
+    "is_renewal": false,
+    "tax_currency": "USD",
+    "tax_registration": null,
+    "object_id": 85471,
+    "content_type": 35,
+    "parent": null,
+    "currency": 1,
+    "country": 233,
+    "coupon": null
 }
 $done({ body: JSON.stringify(objc) });
